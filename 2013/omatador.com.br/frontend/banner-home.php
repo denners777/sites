28e3437fdb5eab4170bb2758949067592dc93c10
Denner Fernandes <denners777@hotmail.com>
2017-02-02 20:54:17 -0200

    matador.art.br
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- 2013/omatador.com.br/frontend/banner-home.php
+++
@@ -1,9 +0,0 @@
-    <section id="homeLogo" class="container_12">
-      <hgroup class="ir">
-        <h1>Matador</h1>
-        <p>TEXTO: RODOLFO SANTANA</p>
-        <p>DIREÇÃO: SUSANA GARCIA e HERSON CAPRI</p>
-        <p>ATORES: DANIEL DIAS DA SILVA e GUSTAVO FALCÃO</p>
-      </hgroup>
-    </section>
-  </div>
--- 2013/omatador.com.br/frontend/banner.php
+++
@@ -1,6 +0,0 @@
-    <section id="banner" class="container_12">
-      <hgroup>
-        <h1 class="ir">Matador</h1>
-        <h2>A PEÇA</h2>
-      </hgroup>
-    </section>
